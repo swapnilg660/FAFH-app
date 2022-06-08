6d95ff30f54aea6b0562a1b98f46e8b47d7f78c3
iam-hbk <rainheritier@gmail.com>
2022-06-08 19:48:13 +0200

    updates

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-
-
+import { BlurView } from "expo-blur";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { useState, useEffect, useCallback } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -14,8 +13,8 @@ import Register from "./Pages/auth/register";
 import ForgotPassword from "./Pages/auth/forgotPassword";
 import Home from "./Pages/home/home";
 
-//custom hooks import
-
+//importing colors
+import colors from "./assets/colors/colors";
 
 export default function App() {
   //route settings
@@ -25,10 +24,28 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {isSignedIn ? (
-        <Stack.Navigator>
+      {!isSignedIn ? (
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: colors.primary },
+            headerTitleStyle: {
+              color: colors.textLight,
+            },
+            headerTintColor: colors.textLight,
+            headerTransparent: true,
+            
+          }}
+        >
           <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen
+            name="Login"
+            options={{
+              headerTitle: "",
+              headerStyle: { backgroundColor: "transparent" },
+              headerTintColor: colors.primary,
+            }}
+            component={Login}
+          />
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         </Stack.Navigator>
