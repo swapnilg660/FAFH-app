5670b3a3784f4eba9a73c8f0f42f17f697c3e0d2
iam-hbk <rainheritier@gmail.com>
2022-09-03 12:59:33 +0200

    working on recipes

--- Pages/app/Home/Recipes.jsx
+++ Pages/app/Home/Recipes.jsx
@@ -1,5 +1,6 @@
 import { AntDesign, Feather, MaterialIcons } from "@expo/vector-icons";
 import {
+  Box,
   Center,
   Heading,
   HStack,
@@ -9,13 +10,16 @@ import {
   Text,
   useTheme,
 } from "native-base";
+import React from "react";
 import { useWindowDimensions } from "react-native";
-import { color } from "react-native-reanimated";
 
 function Recipes({ navigation }) {
   const { height, width } = useWindowDimensions();
   const { colors } = useTheme();
+
+  const [filterList, setFilterList] = React.useState(["All"]);
   const dataFilter = [
+    "All",
     "Indian",
     "Chinese",
     "Italian",
@@ -33,7 +37,7 @@ function Recipes({ navigation }) {
     "Middle Eastern",
     "South American",
     "African",
-  ].sort();
+  ];
   return (
     <ScrollView flex={1} h={height}>
       <HStack space="3" alignItems="center" my={10} mx={5}>
@@ -86,27 +90,62 @@ function Recipes({ navigation }) {
           </Center>
         </Pressable>
       </HStack>
+      {/* Filter carrousel */}
       <HStack>
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           style={{ marginTop: 10 }}
         >
-          {["All", ...dataFilter].map((item, index) => (
+          {[
+            ...filterList,
+            ...dataFilter.filter((item) => !filterList.includes(item)),
+          ].map((item, index) => (
             <Pressable
-              key={index}
-              style={{
-                backgroundColor: colors.primary["600"],
-                borderRadius: 10,
-                padding: 10,
-                marginHorizontal: 5,
+              onPress={() => {
+                if (item != "All") {
+                  setFilterList((prev) => {
+                    if (prev.includes(item)) {
+                      return prev.filter((i) => i !== item);
+                    } else {
+                      return [...prev, item];
+                    }
+                  });
+                }
               }}
+              key={index}
             >
-              <Text style={{ color: colors["white"] }}>{item}</Text>
+              <Center
+                backgroundColor={
+                  filterList.includes(item) ? "primary.600" : "white"
+                }
+                borderWidth={filterList.includes(item) ? 1 : 0}
+                borderColor="primary.600"
+                borderRadius="md"
+                p={2}
+                mx={1}
+                minW={20}
+              >
+                <Text
+                  color={!filterList.includes(item) ? "primary.600" : "white"}
+                >
+                  {item}
+                </Text>
+              </Center>
             </Pressable>
           ))}
         </ScrollView>
       </HStack>
+      {/* Recipes */}
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <HStack mt={10} m={5} space={3} justifyContent="space-around">
+          {[1, 23, 4, 4, 5, 6, 6, 6].map((item, index) => (
+            <Box bg="primary.400" p="12" rounded="lg" key={index}>
+              Box
+            </Box>
+          ))}
+        </HStack>
+      </ScrollView>
     </ScrollView>
   );
 }
