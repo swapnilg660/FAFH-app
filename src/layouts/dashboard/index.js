387209e58e4ecb872260e64c471bf7ee04ccbc0d
suprise079 <ns.ngoveni079@gmail.com>
2022-10-02 16:44:38 +0200

    fixed error

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -42,7 +42,7 @@ function Dashboard() {
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox py={3}>
-        <Grid container spacing={3}>
+        {/* <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
@@ -102,7 +102,7 @@ function Dashboard() {
               />
             </MDBox>
           </Grid>
-        </Grid>
+        </Grid> */}
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6} lg={4}>
