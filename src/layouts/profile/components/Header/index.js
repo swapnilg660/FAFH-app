415e36a8004f42b5068ddca3d26576475e15fb94
suprise079 <ns.ngoveni079@gmail.com>
2022-10-03 09:51:49 +0200

    hidden profile page

--- src/layouts/profile/components/Header/index.js
+++ src/layouts/profile/components/Header/index.js
@@ -99,10 +99,10 @@ function Header({ children }) {
           <Grid item>
             <MDBox height="100%" mt={0.5} lineHeight={1}>
               <MDTypography variant="h5" fontWeight="medium">
-                Richard Davis
+                Suprise Ngoveni
               </MDTypography>
               <MDTypography variant="button" color="text" fontWeight="regular">
-                CEO / Co-Founder
+                Software developer
               </MDTypography>
             </MDBox>
           </Grid>
