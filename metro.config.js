ab5331ffdb1876edee3ecf4565481deade764005
SupriseN <suprise.ngoveni@ayo4u.com>
2024-05-29 18:37:21 +0200

    Updated metro file

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,24 @@
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  //added this
-  resolver: {
-    sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
-  },
-};
+const { getDefaultConfig } = require("@expo/metro-config");
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  const defaultConfig = await getDefaultConfig(__dirname);
+
   return {
     transformer: {
+      ...defaultConfig.transformer,
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
       assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     },
     resolver: {
+      ...defaultConfig.resolver,
+      sourceExts: [...defaultConfig.resolver.sourceExts, "jsx", "js", "ts", "tsx", "cjs"],
       extraNodeModules: {
+        ...defaultConfig.resolver.extraNodeModules,
         "react-native-gesture-handler": require.resolve("react-native-gesture-handler"),
       },
     },
